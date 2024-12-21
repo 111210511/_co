66ae4dc56d31a560c391e7ee307bd73343e0604d
leemax0617 <leemax0617@gmail.com>
2024-12-21 17:15:05 +0800

    add ...

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,63 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// This file is part of www.nand2tetris.org
+// Implements screen fill/unfill based on keyboard input
+// When any key is pressed, the screen should become black
+// When no key is pressed, the screen should become white
+
+(INFINITE_LOOP)
+    // Initialize screen fill color to white (0)
+    @color
+    M=0
+
+    // Check if any key is pressed
+    @KBD
+    D=M
+    @FILL_SCREEN
+    D;JEQ        // If no key pressed, jump to fill (will fill with white)
+
+    // If key is pressed, set color to black (-1)
+    @color
+    M=-1
+
+(FILL_SCREEN)
+    // Initialize pixel counter to 8192 (number of 16-bit words in the screen)
+    @8192
+    D=A
+    @counter
+    M=D
+
+    // Set base screen address
+    @SCREEN
+    D=A
+    @address
+    M=D
+
+(FILL_LOOP)
+    // Load the color to fill with
+    @color
+    D=M
+
+    // Fill current word with color
+    @address
+    A=M
+    M=D
+
+    // Increment address
+    @address
+    M=M+1
+
+    // Decrement counter
+    @counter
+    M=M-1
+    D=M
+
+    // If counter > 0, continue filling
+    @FILL_LOOP
+    D;JGT
+
+    // Return to infinite loop to check keyboard again
+    @INFINITE_LOOP
+    0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,44 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2] respectively.)
+// The algorithm uses repeated addition: R2 = R0 * R1
+
+// Initialize R2 to 0
+@R2
+M=0
+
+// Initialize i (R3) to R0 (number of times to add)
+@R0
+D=M
+@R3
+M=D
+
+// If R0 is 0, skip to end
+@END
+D;JEQ
+
+// Main loop
+(LOOP)
+    // Load R1 into D
+    @R1
+    D=M
+
+    // Add R1 to R2
+    @R2
+    M=M+D
+
+    // Decrement counter (R3)
+    @R3
+    M=M-1
+    D=M
+
+    // If counter > 0, continue loop
+    @LOOP
+    D;JGT
+
+(END)
+    @END
+    0;JMP
